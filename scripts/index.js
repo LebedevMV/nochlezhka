a5e5ef96dd0f375d514084bc03165c27b49e3dac
grigarazH <45718270+grigarazH@users.noreply.github.com>
2021-12-05 16:27:11 +0300

    Реализовал стили выпадающего списка

---
+++ scripts/index.js
@@ -0,0 +1,53 @@
+const createCustomOptionElement = (optionElement) => {
+  const customOptionElement = document.querySelector(".select-option-template").content.firstElementChild.cloneNode(true);
+  customOptionElement.dataset.value = optionElement.value;
+  customOptionElement.textContent = optionElement.text;
+  return customOptionElement;
+}
+
+const showErrorForSelect = (selectContainer) => {
+  const errorLabel = selectContainer.nextElementSibling;
+  errorLabel.style.display = "flex";
+}
+
+const hideErrorForSelect = (selectContainer) => {
+  const errorLabel = selectContainer.nextElementSibling;
+  errorLabel.style.display = "none";
+}
+
+const fillSelectContainer = (selectContainer) => {
+  const selectElement = selectContainer.querySelector(".form__input_type_select");
+  const options = selectElement.querySelectorAll(".form__select-option");
+  const selectOptionContainer = selectContainer.querySelector(".form__select-option-container");
+  options.forEach(option => {
+    const customOptionElement = createCustomOptionElement(option);
+    customOptionElement.addEventListener("click", () => {
+      selectElement.value = customOptionElement.dataset.value;
+      selectOptionContainer.classList.remove("form__select-option-container_active");
+      selectElement.dispatchEvent(new Event("change"));
+    });
+    selectOptionContainer.append(customOptionElement);
+  });
+  const pseudoOptions = selectOptionContainer.querySelectorAll(".form__select-pseudo-option");
+  selectElement.addEventListener("focus", () => {
+    selectOptionContainer.classList.add("form__select-option-container_active");
+  });
+  selectElement.addEventListener("change", () => {
+    console.log("changingValue");
+    hideErrorForSelect(selectContainer);
+    pseudoOptions.forEach(option => {
+      option.classList.remove("form__select-pseudo-option_checked");
+    });
+    console.log(`form__select-pseudo-option[data-value="${selectElement.value}"]`);
+    const selectedOption = selectOptionContainer.querySelector(`.form__select-pseudo-option[data-value="${selectElement.value}"]`);
+    selectedOption.classList.add("form__select-pseudo-option_checked");
+  });
+  selectElement.addEventListener("invalid", () => {
+    showErrorForSelect(selectContainer);
+  });
+  selectElement.checkValidity();
+}
+
+const selectContainers = document.querySelectorAll(".form__select-container");
+
+selectContainers.forEach(selectContainer => fillSelectContainer(selectContainer));
